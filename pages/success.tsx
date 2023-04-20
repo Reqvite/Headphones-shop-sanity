4f58a8bcd64ea4a521432336d8e4076f858c954f
Reqvite <reqvite2233@gmail.com>
2023-04-20 18:40:32 +0300

    Update success.tsx

--- pages/success.tsx
+++ pages/success.tsx
@@ -22,22 +22,17 @@ const Success: FC = () => {
     }
   }, [router, setCartItems, setTotalPrice, setTotalQuantities, status]);
 
-  if (status !== "true") {
-    return (
-      <div className="success-wrapper">
-        <div className="success">
-          <h2>You haven&apos;t completed your purchase yet.</h2>
-        </div>
-      </div>
-    );
-  }
   return (
     <div className="success-wrapper">
       <div className="success">
         <p className="icon">
           <BsBagCheckFill />
         </p>
-        <h2>Thank you for your order!</h2>
+        <h2>
+          {status !== "true"
+            ? "You haven&apos;t completed your purchase yet."
+            : "Thank you for your order!"}
+        </h2>
         <p className="email-msg">Check your email inbox for the receipt.</p>
         <p className="description">
           If you have any questions, please email
